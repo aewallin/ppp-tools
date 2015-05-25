a21599ff37556cdebb7eaa001633800a7a01e50c
Anders Wallin <anders.e.e.wallin@gmail.com>
2015-05-25 21:15:56 +0300

    helper libraries

---
+++ gpstime.py
@@ -0,0 +1,225 @@
+""" 
+  2  A Python implementation of GPS related time conversions. 
+  3   
+  4  Copyright 2002 by Bud P. Bruegger, Sistema, Italy 
+  5  mailto:bud@sistema.it 
+  6  http://www.sistema.it 
+  7   
+  8  Modifications for GPS seconds by Duncan Brown 
+  9   
+ 10  PyUTCFromGpsSeconds added by Ben Johnson 
+ 11   
+ 12  This program is free software; you can redistribute it and/or modify it under 
+ 13  the terms of the GNU Lesser General Public License as published by the Free 
+ 14  Software Foundation; either version 2 of the License, or (at your option) any 
+ 15  later version. 
+ 16   
+ 17  This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ 18  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ 19  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more 
+ 20  details. 
+ 21   
+ 22  You should have received a copy of the GNU Lesser General Public License along 
+ 23  with this program; if not, write to the Free Software Foundation, Inc., 59 
+ 24  Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ 25   
+ 26  GPS Time Utility functions 
+ 27   
+ 28  This file contains a Python implementation of GPS related time conversions. 
+ 29   
+ 30  The two main functions convert between UTC and GPS time (GPS-week, time of 
+ 31  week in seconds, GPS-day, time of day in seconds).  The other functions are 
+ 32  convenience wrappers around these base functions.   
+ 33   
+ 34  A good reference for GPS time issues is: 
+ 35  http://www.oc.nps.navy.mil/~jclynch/timsys.html 
+ 36   
+ 37  Note that python time types are represented in seconds since (a platform 
+ 38  dependent Python) Epoch.  This makes implementation quite straight forward 
+ 39  as compared to some algorigthms found in the literature and on the web.   
+ 40  """ 
+
+import time, math 
+import datetime
+
+secsInWeek = 604800 
+secsInDay = 86400 
+gpsEpoch = (1980, 1, 6, 0, 0, 0)  # (year, month, day, hh, mm, ss) 
+   
+def dayOfWeek(year, month, day):
+    "returns day of week: 0=Sun, 1=Mon, .., 6=Sat" 
+    hr = 12 #make sure you fall into right day, middle is save 
+    t = time.mktime((year, month, day, hr, 0, 0, 0, 0, -1)) 
+    pyDow = time.localtime(t)[6] 
+    gpsDow = (pyDow + 1) % 7
+    return gpsDow
+ 
+def gpsWeek(year, month, day):
+    "returns (full) gpsWeek for given date (in UTC)"  
+    hr = 12 #make sure you fall into right day, middle is save 
+    return gpsFromUTC(year, month, day, hr, 0, 0.0)[0]
+
+def julianDay(year, month, day):
+    "returns julian day=day since Jan 1 of year"
+    hr = 12 #make sure you fall into right day, middle is save 
+    t = time.mktime((year, month, day, hr, 0, 0, 0, 0, -1)) 
+    julDay = time.localtime(t)[7] # tm_yday
+    return julDay
+
+def dateFromJulian( year, julian_day ):
+    t0 = datetime.datetime( year, 1, 1 ) # first day of year
+    t0 = t0 + datetime.timedelta( days=julian_day-1 )
+    return t0
+
+def mkUTC(year, month, day, hour, min, sec):
+    "similar to python's mktime but for utc" 
+    spec = [year, month, day, hour, min, int(sec)] + [0, 0, 0] 
+    utc = time.mktime(spec) - time.timezone 
+    return utc
+
+def ymdhmsFromPyUTC(pyUTC):
+    "returns tuple from a python time value in UTC" 
+    ymdhmsXXX = time.gmtime(pyUTC) 
+    return ymdhmsXXX[:-3]
+
+def wtFromUTCpy(pyUTC, leapSecs=14):
+    """convenience function: 87 allows to use python UTC times and 
+    returns only week and tow""" 
+    ymdhms = ymdhmsFromPyUTC(pyUTC) 
+    wSowDSoD = apply(gpsFromUTC, ymdhms + (leapSecs,)) 
+    return wSowDSoD[0:2]
+
+def gpsFromUTC(year, month, day, hour, min, sec, leapSecs=14):
+    """converts UTC to: gpsWeek, secsOfWeek, gpsDay, secsOfDay 95 96 
+    a good reference is: http://www.oc.nps.navy.mil/~jclynch/timsys.html 97 98 
+    This is based on the following facts (see reference above): 99 100 
+    GPS time is basically measured in (atomic) seconds since 101 
+    January 6, 1980, 00:00:00.0 (the GPS Epoch) 102 103 
+    The GPS week starts on Saturday midnight (Sunday morning), and runs 104 
+    for 604800 seconds. 105 106 
+    Currently, GPS time is 13 seconds ahead of UTC (see above reference). 107 
+    While GPS SVs transmit this difference and the date when another leap 108 
+    second takes effect, the use of leap seconds cannot be predicted. This 109 
+    routine is precise until the next leap second is introduced and has to be 110 
+    updated after that. 111 112 
+    SOW = Seconds of Week 113 
+    SOD = Seconds of Day 114 115 N
+    ote: Python represents time in integer seconds, fractions are lost!!! 116 
+    """  
+    secFract = sec % 1 
+    epochTuple = gpsEpoch + (-1, -1, 0) 
+    t0 = time.mktime(epochTuple) 
+    t = time.mktime((year, month, day, hour, min, int(sec), -1, -1, 0)) 
+    # Note: time.mktime strictly works in localtime and to yield UTC, it should be 122 
+    # corrected with time.timezone 123 
+    # However, since we use the difference, this correction is unnecessary. 124 
+    # Warning: trouble if daylight savings flag is set to -1 or 1 !!! 125 
+    t = t + leapSecs 
+    tdiff = t - t0 
+    gpsSOW = (tdiff % secsInWeek) + secFract 
+    gpsWeek = int(math.floor(tdiff/secsInWeek)) 
+    gpsDay = int(math.floor(gpsSOW/secsInDay)) 
+    gpsSOD = (gpsSOW % secsInDay) 
+    return (gpsWeek, gpsSOW, gpsDay, gpsSOD)
+
+def UTCFromGps(gpsWeek, SOW, leapSecs=14):
+    """converts gps week and seconds to UTC 136 137 
+    see comments of inverse function! 138 139 
+    SOW = seconds of week 140 
+    gpsWeek is the full number (not modulo 1024) 141 
+    """ 
+    secFract = SOW % 1 
+    epochTuple = gpsEpoch + (-1, -1, 0) 
+    t0 = time.mktime(epochTuple) - time.timezone #mktime is localtime, correct for UTC 145 
+    tdiff = (gpsWeek * secsInWeek) + SOW - leapSecs 
+    t = t0 + tdiff 
+    (year, month, day, hh, mm, ss, dayOfWeek, julianDay, daylightsaving) = time.gmtime(t) 
+    #use gmtime since localtime does not allow to switch off daylighsavings correction!!! 149 
+    return (year, month, day, hh, mm, ss + secFract)
+
+def GpsSecondsFromPyUTC( pyUTC, leapSecs=14 ):
+    """converts the python epoch to gps seconds 153 154 
+    pyEpoch = the python epoch from time.time() 155 
+    """ 
+    t = t=gpsFromUTC(*ymdhmsFromPyUTC( pyUTC )) 
+    return int(t[0] * 60 * 60 * 24 * 7 + t[1])
+
+def PyUTCFromGpsSeconds(gpsseconds):
+    """converts gps seconds to the 161 python epoch. 
+    That is, the time 162 that would be returned from time.time() 163 
+    at gpsseconds. 164 
+    """ 
+    pyUTC
+
+#===== Tests  ========================================= 
+
+def testTimeStuff():
+    print "-"*20 
+    print 
+    print "The GPS Epoch when everything began (1980, 1, 6, 0, 0, 0, leapSecs=0)" 
+    (w, sow, d, sod) = gpsFromUTC(1980, 1, 6, 0, 0, 0, leapSecs=0) 
+    print "**** week: %s, sow: %s, day: %s, sod: %s" % (w, sow, d, sod) 
+    print " and hopefully back:" 
+    print "**** %s, %s, %s, %s, %s, %s\n" % UTCFromGps(w, sow, leapSecs=0)
+    print "The time of first Rollover of GPS week (1999, 8, 21, 23, 59, 47)" 
+    (w, sow, d, sod) = gpsFromUTC(1999, 8, 21, 23, 59, 47) 
+    print "**** week: %s, sow: %s, day: %s, sod: %s" % (w, sow, d, sod) 
+    print " and hopefully back:" 
+    print "**** %s, %s, %s, %s, %s, %s\n" % UTCFromGps(w, sow, leapSecs=14)
+    
+    print "Today is GPS week 1186, day 3, seems to run ok (2002, 10, 2, 12, 6, 13.56)" 
+    (w, sow, d, sod) = gpsFromUTC(2002, 10, 2, 12, 6, 13.56) 
+    print "**** week: %s, sow: %s, day: %s, sod: %s" % (w, sow, d, sod) 
+    print " and hopefully back:" 
+    print "**** %s, %s, %s, %s, %s, %s\n" % UTCFromGps(w, sow)
+
+def testJulD():
+    print "testJulD()"
+    print '2002, 10, 11 -> 284 ==??== ', julianDay(2002, 10, 11)
+    print " "
+    
+def testGpsWeek():
+    print "testGpsWeek()"
+    print '2002, 10, 11 -> 1187 ==??== ', gpsWeek(2002, 10, 11)
+    print " "
+    
+def testDayOfWeek():
+    print "testDayOfWeek()"
+    print '2002, 10, 12 -> 6 ==??== dayOfWeek = ', dayOfWeek(2002, 10, 12) 
+    print '2002, 10, 6 -> 0 ==??== dayOfWeek =', dayOfWeek(2002, 10, 6)
+    print " "
+
+def testNow():
+    now = datetime.datetime.now() - datetime.timedelta(days=2)
+    #now = datetime.datetime(2014,2,23 ) # doy 54
+    print "testNow()"
+    print " now = ", now
+    print " Julian day      = ", julianDay( now.year, now.month, now.day )
+    print " GPS week        = ", gpsWeek( now.year, now.month, now.day) 
+    print " GPS day of week = ", dayOfWeek( now.year, now.month, now.day) 
+    print " date from Julian= ", dateFromJulian( now.year, julianDay(now.year, now.month, now.day) )
+    print '2002, 10, 6 -> 0 ==??== dayOfWeek =', dayOfWeek(2014, 01, 9)
+    
+def testPyUtilties():
+    print "testDayOfWeek()"
+    ymdhms = (2014, 1, 9, 8, 34, 12.3) 
+    print "testing for: ", ymdhms 
+    pyUtc = apply(mkUTC, ymdhms) 
+    back = ymdhmsFromPyUTC(pyUtc) 
+    print "yields : ", back 
+    #*********************** !!!!!!!! 
+    #assert(ymdhms == back) 208 
+    #! TODO: this works only with int seconds!!! fix!!! 
+    (w, t) = wtFromUTCpy(pyUtc) 
+    print "week and time: ", (w,t)
+    print " "
+
+#===== Main ========================================= 
+if __name__ == "__main__": 
+    pass 
+    testTimeStuff() 
+    testGpsWeek() 
+    testJulD() 
+    testDayOfWeek() 
+    testPyUtilties() 
+    testNow()
---
+++ jdutil.py
@@ -0,0 +1,466 @@
+"""
+Functions for converting dates to/from JD and MJD. Assumes dates are historical
+dates, including the transition from the Julian calendar to the Gregorian
+calendar in 1582. No support for proleptic Gregorian/Julian calendars.
+
+:Author: Matt Davis
+:Website: http://github.com/jiffyclub
+
+see http://en.wikipedia.org/wiki/Julian_day
+
+"""
+
+import math
+import datetime as dt
+import pytz
+
+# Note: The Python datetime module assumes an infinitely valid Gregorian calendar.
+#       The Gregorian calendar took effect after 10-15-1582 and the dates 10-05 through
+#       10-14-1582 never occurred. Python datetime objects will produce incorrect
+#       time deltas if one date is from before 10-15-1582.
+
+# AW 2014-01-24
+# 
+def dt2mjd( dtlist ):
+    """
+    convert list of datetimes to list of MJDs
+    """
+    mjdlist=[]
+    for dt in dtlist:
+        mjdlist.append( jd_to_mjd( datetime_to_jd( dt ) ) )
+    return mjdlist
+
+def stamp2dt( stamplist ):
+    """
+    convert list of time-stamps to list of datetimes
+    """
+    dtlist = []
+    for t in stamplist:
+        dtlist.append( datetime.fromtimestamp(t, tz=pytz.UTC) )
+    return dtlist
+
+def mjd_to_datetime(mjd):
+    """
+    convert mjd to datetime
+    """
+    return jd_to_datetime( mjd_to_jd( mjd ) )
+
+def mjd_to_jd(mjd):
+    """
+    Convert Modified Julian Day to Julian Day.
+        
+    Parameters
+    ----------
+    mjd : float
+        Modified Julian Day
+        
+    Returns
+    -------
+    jd : float
+        Julian Day
+    
+        
+    """
+    return mjd + 2400000.5
+
+    
+def jd_to_mjd(jd):
+    """
+    Convert Julian Day to Modified Julian Day
+    
+    Parameters
+    ----------
+    jd : float
+        Julian Day
+        
+    Returns
+    -------
+    mjd : float
+        Modified Julian Day
+    
+    """
+    return jd - 2400000.5
+
+    
+def date_to_jd(year,month,day):
+    """
+    Convert a date to Julian Day.
+    
+    Algorithm from 'Practical Astronomy with your Calculator or Spreadsheet', 
+        4th ed., Duffet-Smith and Zwart, 2011.
+    
+    Parameters
+    ----------
+    year : int
+        Year as integer. Years preceding 1 A.D. should be 0 or negative.
+        The year before 1 A.D. is 0, 10 B.C. is year -9.
+        
+    month : int
+        Month as integer, Jan = 1, Feb. = 2, etc.
+    
+    day : float
+        Day, may contain fractional part.
+    
+    Returns
+    -------
+    jd : float
+        Julian Day
+        
+    Examples
+    --------
+    Convert 6 a.m., February 17, 1985 to Julian Day
+    
+    >>> date_to_jd(1985,2,17.25)
+    2446113.75
+    
+    """
+    if month == 1 or month == 2:
+        yearp = year - 1
+        monthp = month + 12
+    else:
+        yearp = year
+        monthp = month
+    
+    # this checks where we are in relation to October 15, 1582, the beginning
+    # of the Gregorian calendar.
+    if ((year < 1582) or
+        (year == 1582 and month < 10) or
+        (year == 1582 and month == 10 and day < 15)):
+        # before start of Gregorian calendar
+        B = 0
+    else:
+        # after start of Gregorian calendar
+        A = math.trunc(yearp / 100.)
+        B = 2 - A + math.trunc(A / 4.)
+        
+    if yearp < 0:
+        C = math.trunc((365.25 * yearp) - 0.75)
+    else:
+        C = math.trunc(365.25 * yearp)
+        
+    D = math.trunc(30.6001 * (monthp + 1))
+    
+    jd = B + C + D + day + 1720994.5
+    
+    return jd
+    
+    
+def jd_to_date(jd):
+    """
+    Convert Julian Day to date.
+    
+    Algorithm from 'Practical Astronomy with your Calculator or Spreadsheet', 
+        4th ed., Duffet-Smith and Zwart, 2011.
+    
+    Parameters
+    ----------
+    jd : float
+        Julian Day
+        
+    Returns
+    -------
+    year : int
+        Year as integer. Years preceding 1 A.D. should be 0 or negative.
+        The year before 1 A.D. is 0, 10 B.C. is year -9.
+        
+    month : int
+        Month as integer, Jan = 1, Feb. = 2, etc.
+    
+    day : float
+        Day, may contain fractional part.
+        
+    Examples
+    --------
+    Convert Julian Day 2446113.75 to year, month, and day.
+    
+    >>> jd_to_date(2446113.75)
+    (1985, 2, 17.25)
+    
+    """
+    jd = jd + 0.5
+    
+    F, I = math.modf(jd)
+    I = int(I)
+    
+    A = math.trunc((I - 1867216.25)/36524.25)
+    
+    if I > 2299160:
+        B = I + 1 + A - math.trunc(A / 4.)
+    else:
+        B = I
+        
+    C = B + 1524
+    
+    D = math.trunc((C - 122.1) / 365.25)
+    
+    E = math.trunc(365.25 * D)
+    
+    G = math.trunc((C - E) / 30.6001)
+    
+    day = C - E + F - math.trunc(30.6001 * G)
+    
+    if G < 13.5:
+        month = G - 1
+    else:
+        month = G - 13
+        
+    if month > 2.5:
+        year = D - 4716
+    else:
+        year = D - 4715
+        
+    return year, month, day
+    
+    
+def hmsm_to_days(hour=0,min=0,sec=0,micro=0):
+    """
+    Convert hours, minutes, seconds, and microseconds to fractional days.
+    
+    Parameters
+    ----------
+    hour : int, optional
+        Hour number. Defaults to 0.
+    
+    min : int, optional
+        Minute number. Defaults to 0.
+    
+    sec : int, optional
+        Second number. Defaults to 0.
+    
+    micro : int, optional
+        Microsecond number. Defaults to 0.
+        
+    Returns
+    -------
+    days : float
+        Fractional days.
+        
+    Examples
+    --------
+    >>> hmsm_to_days(hour=6)
+    0.25
+    
+    """
+    days = sec + (micro / 1.e6)
+    
+    days = min + (days / 60.)
+    
+    days = hour + (days / 60.)
+    
+    return days / 24.
+    
+    
+def days_to_hmsm(days):
+    """
+    Convert fractional days to hours, minutes, seconds, and microseconds.
+    Precision beyond microseconds is rounded to the nearest microsecond.
+    
+    Parameters
+    ----------
+    days : float
+        A fractional number of days. Must be less than 1.
+        
+    Returns
+    -------
+    hour : int
+        Hour number.
+    
+    min : int
+        Minute number.
+    
+    sec : int
+        Second number.
+    
+    micro : int
+        Microsecond number.
+        
+    Raises
+    ------
+    ValueError
+        If `days` is >= 1.
+        
+    Examples
+    --------
+    >>> days_to_hmsm(0.1)
+    (2, 24, 0, 0)
+    
+    """
+    hours = days * 24.
+    hours, hour = math.modf(hours)
+    
+    mins = hours * 60.
+    mins, min = math.modf(mins)
+    
+    secs = mins * 60.
+    secs, sec = math.modf(secs)
+    
+    micro = round(secs * 1.e6)
+    
+    return int(hour), int(min), int(sec), int(micro)
+    
+
+def datetime_to_jd(date):
+    """
+    Convert a `datetime.datetime` object to Julian Day.
+    
+    Parameters
+    ----------
+    date : `datetime.datetime` instance
+    
+    Returns
+    -------
+    jd : float
+        Julian day.
+        
+    Examples
+    --------
+    >>> d = datetime.datetime(1985,2,17,6)  
+    >>> d
+    datetime.datetime(1985, 2, 17, 6, 0)
+    >>> jdutil.datetime_to_jd(d)
+    2446113.75
+    
+    """
+    days = date.day + hmsm_to_days(date.hour,date.minute,date.second,date.microsecond)
+    
+    return date_to_jd(date.year,date.month,days)
+    
+    
+def jd_to_datetime(jd):
+    """
+    Convert a Julian Day to an `jdutil.datetime` object.
+    
+    Parameters
+    ----------
+    jd : float
+        Julian day.
+        
+    Returns
+    -------
+    dt : `jdutil.datetime` object
+        `jdutil.datetime` equivalent of Julian day.
+    
+    Examples
+    --------
+    >>> jd_to_datetime(2446113.75)
+    datetime(1985, 2, 17, 6, 0)
+    
+    """
+    year, month, day = jd_to_date(jd)
+    
+    frac_days,day = math.modf(day)
+    day = int(day)
+    
+    hour,min,sec,micro = days_to_hmsm(frac_days)
+    
+    return datetime(year,month,day,hour,min,sec,micro)
+
+
+def timedelta_to_days(td):
+    """
+    Convert a `datetime.timedelta` object to a total number of days.
+    
+    Parameters
+    ----------
+    td : `datetime.timedelta` instance
+    
+    Returns
+    -------
+    days : float
+        Total number of days in the `datetime.timedelta` object.
+        
+    Examples
+    --------
+    >>> td = datetime.timedelta(4.5)
+    >>> td
+    datetime.timedelta(4, 43200)
+    >>> timedelta_to_days(td)
+    4.5
+    
+    """
+    seconds_in_day = 24. * 3600.
+    
+    days = td.days + (td.seconds + (td.microseconds * 10.e6)) / seconds_in_day
+    
+    return days
+    
+    
+class datetime(dt.datetime):
+    """
+    A subclass of `datetime.datetime` that performs math operations by first
+    converting to Julian Day, then back to a `jdutil.datetime` object.
+    
+    Addition works with `datetime.timedelta` objects, subtraction works with
+    `datetime.timedelta`, `datetime.datetime`, and `jdutil.datetime` objects.
+    Not all combinations work in all directions, e.g.
+    `timedelta - datetime` is meaningless.
+    
+    See Also
+    --------
+    datetime.datetime : Parent class.
+    
+    """
+    def __add__(self,other):
+        if not isinstance(other,dt.timedelta):
+            s = "jdutil.datetime supports '+' only with datetime.timedelta"
+            raise TypeError(s)
+        
+        days = timedelta_to_days(other)
+        
+        combined = datetime_to_jd(self) + days
+        
+        return jd_to_datetime(combined)
+        
+    def __radd__(self,other):
+        if not isinstance(other,dt.timedelta):
+            s = "jdutil.datetime supports '+' only with datetime.timedelta"
+            raise TypeError(s)
+        
+        days = timedelta_to_days(other)
+        
+        combined = datetime_to_jd(self) + days
+        
+        return jd_to_datetime(combined)
+        
+    def __sub__(self,other):
+        if isinstance(other,dt.timedelta):
+            days = timedelta_to_days(other)
+            
+            combined = datetime_to_jd(self) - days
+            
+            return jd_to_datetime(combined)
+            
+        elif isinstance(other, (datetime,dt.datetime)):
+            diff = datetime_to_jd(self) - datetime_to_jd(other)
+            
+            return dt.timedelta(diff)
+            
+        else:
+            s = "jdutil.datetime supports '-' with: "
+            s += "datetime.timedelta, jdutil.datetime and datetime.datetime"
+            raise TypeError(s)
+            
+    def __rsub__(self,other):
+        if not isinstance(other, (datetime,dt.datetime)):
+            s = "jdutil.datetime supports '-' with: "
+            s += "jdutil.datetime and datetime.datetime"
+            raise TypeError(s)
+            
+        diff = datetime_to_jd(other) - datetime_to_jd(self)
+            
+        return dt.timedelta(diff)
+        
+    def to_jd(self):
+        """
+        Return the date converted to Julian Day.
+        
+        """
+        return datetime_to_jd(self)
+        
+    def to_mjd(self):
+        """
+        Return the date converted to Modified Julian Day.
+        
+        """
+        return jd_to_mjd(self.to_jd())
+    
