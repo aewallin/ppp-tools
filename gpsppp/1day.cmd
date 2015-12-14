7392926ddb36287f26ae5d3f80dc2dff4063de8b
Anders Wallin <anders.e.e.wallin@gmail.com>
2015-12-15 00:00:45 +0200

    add gpsppp files

---
+++ gpsppp/1day.cmd
@@ -0,0 +1,21 @@
+' UT DAYS OBSERVED                      (1-45)'               2
+' USER DYNAMICS         (1=STATIC,2=KINEMATIC)'               1
+' OBSERVATION TO PROCESS         (1=COD,2=C&P)'               2
+' FREQUENCY TO PROCESS        (1=L1,2=L2,3=L3)'               3
+' SATELLITE EPHEMERIS INPUT     (1=BRD ,2=SP3)'               2
+' SATELLITE PRODUCT    (1=NO,2=PrcClk,3=MRTCA)'               2
+' SATELLITE CLOCK INTERPOLATION   (1=NO,2=YES)'               1
+' IONOSPHERIC GRID INPUT          (1=NO,2=YES)'               1
+' SOLVE STATION COORDINATES       (1=NO,2=YES)'               2
+' SOLVE TROP. (1=NO,2-5=RW MM/HR) (+100=grad) '               5
+' BACKWARD SUBSTITUTION           (1=NO,2=YES)'               2
+' REFERENCE SYSTEM            (1=NAD83,2=ITRF)'               2
+' COORDINATE SYSTEM(1=ELLIPSOIDAL,2=CARTESIAN)'               1
+' A-PRIORI PSEUDORANGE SIGMA               (m)'           2.000
+' A-PRIORI CARRIER PHASE SIGMA             (m)'            .015
+' LATITUDE  (ddmmss.sss,+N) or ECEF X      (m)'           0.000
+' LONGITUDE (ddmmss.sss,+E) or ECEF Y      (m)'           0.000
+' HEIGHT (m)                or ECEF Z      (m)'           0.000
+' ANTENNA HEIGHT                           (m)'           0.000
+' CUTOFF ELEVATION                       (deg)'          10.000
+' GDOP CUTOFF                                 '          20.000
