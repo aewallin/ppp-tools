98f5c0d4acda5903a5c7a3e05b69ba307748f277
Anders Wallin <anders.e.e.wallin@gmail.com>
2021-06-30 16:49:00 +0300

    cmd-file

---
+++ gpsppp/2day.cmd
@@ -0,0 +1,21 @@
+' UT DAYS OBSERVED                      (1-45)'                   3
+' USER DYNAMICS         (1=STATIC,2=KINEMATIC)'                   1
+' OBSERVATION TO PROCESS         (1=COD,2=C&P)'                   2
+' FREQUENCY TO PROCESS        (1=L1,2=L2,3=L3)'                   3
+' SATELLITE EPHEMERIS INPUT     (1=BRD ,2=SP3)'                   2
+' SAT CLOCKS(1=NO,2=Prc,3=RTCA,4=RTCM,+10=!AR)'                   2
+' SATELLITE CLOCK INTERPOLATION   (1=NO,2=YES)'                   1
+' IONOSPHERIC GRID (1=NO,2=YES,3=INIT,+10*MFi)'                   1
+' SOLVE STATION COORDINATES       (1=NO,2=YES)'                   2
+' SOLVE TROP. (1=NO,2-5=RW MM/HR)  (+100=grad)'                 105
+' BACKWARD SUBSTITUTION    (1=NO,2=YES,3=!CLK)'                   2
+' REFERENCE SYSTEM            (1=NAD83,2=ITRF)'                   2
+' COORDINATE SYSTEM(1=ELLIPSOIDAL,2=CARTESIAN)'                   1
+' A-PRIORI PSEUDORANGE SIGMA(m)    [&MISC TOL]'              5.0000   9.00
+' A-PRIORI CARRIER PHASE SIGMA(m)  [&MISC TOL]'               .0100   9.00
+' LAT(ddmmss.sss,+N) | ECEF X(m) [&VEL(mm/yr)]'              0.0000   0.00
+' LON(ddmmss.sss,+E) | ECEF Y(m) [&VEL(mm/yr)]'              0.0000   0.00
+' HEIGHT (m)         | ECEF Z(m) [&VEL(mm/yr)]'              0.0000   0.00
+' ANTENNA HEIGHT                           (m)'              0.0000
+' CUTOFF ELEVATION                       (deg)'              5.0000
+' GDOP CUTOFF                                 '             20.0000
